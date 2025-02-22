7fc9b750335c6313f782e3b17c73d37f48c7dfac
JavaZero <71128095+JavaZeroo@users.noreply.github.com>
2025-02-22 19:49:32 +0800

    Update app_install.ps1

--- app_install.ps1
+++ app_install.ps1
@@ -1,1 +1,1 @@
-winget install Valve.Steam AgileBits.1Password  Microsoft.PowerToys  wechat idm spotify vscode Git.Git startallback starship todesk Telegram.TelegramDesktop
+winget install Valve.Steam AgileBits.1Password  Microsoft.PowerToys  wechat idm spotify vscode Git.Git startallback starship todesk Telegram.TelegramDesktop 火绒安全软件 Microsoft.PowerShell
